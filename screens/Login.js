5ff118c6c8977a2f040d66ddd3d4c9a446a044f9
Nduka O. Queendalin <Ndkkqueenie@users.noreply.github.com>
2021-08-09 02:00:08 +0100

    new setup

---
+++ screens/Login.js
@@ -0,0 +1,23 @@
+import React from 'react';
+import {
+    View,
+    Text,
+    TouchableOpacity,
+    ImageBackground,
+    StatusBar
+} from 'react-native';
+
+import { images, COLORS, SIZES, FONTS } from "../constants";
+
+const Login = ({ navigation }) => {
+    return (
+        <View
+        >
+            {/* Header */}
+
+            {/* Detail */}
+        </View>
+    )
+}
+
+export default Login;
